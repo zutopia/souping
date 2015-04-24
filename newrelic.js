cd95b983344c657667ddaeacc2d4cc23b8c1c834
fucking name <391400311@qq.com>
2015-04-24 15:49:29 +0800

    Create newrelic.js

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['sp'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '99331460b90f0ecbc2f2ba20a27eaa160e3847ae',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
