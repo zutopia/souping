07b17e5de3bb4cf12c49ed4edca8b0960ed30abe
panhb <391400311@qq.com>
2015-07-21 12:06:27 +0800

    fix bug

--- routes/index.js
+++ routes/index.js
@@ -51,7 +51,7 @@ router.get('/main', function(req, res) {
 	var params = Url.parse(req.url,true).query; 
 	var key = params.key;
 	console.log(key);
-	if(key === '' || key === null ){
+	if(typeof(key) === 'undefined' || key === '' || key === null ){
 		res.render('error');
 	} else {
 		superagent.get('https://api.douban.com/v2/movie/search?q='+key).end(function (err, sres) {
@@ -118,170 +118,174 @@ router.get('/main', function(req, res) {
 router.get('/detail', function(req, res) {
 	var params = Url.parse(req.url,true).query; 
 	var id = params.id;
-	var subtype = '';
-	var title = '';
-	superagent.get('https://api.douban.com/v2/movie/subject/'+id).end(function (err, m) {
-		var s_obj = JSON.parse(m.text);
-		var douban_url = s_obj.alt;
-		var movie_obj = {};
-		subtype = s_obj.subtype;
-		title = s_obj.title;
-		//链接
-		movie_obj.douban_url = s_obj.alt;
-		//封面图
-		movie_obj.douban_src = s_obj.images.large;
-		//评分
-		movie_obj.douban_score = s_obj.rating.average;
-		var score_arr = (s_obj.rating.average+'').split('.');
-		movie_obj.douban_score_1 = score_arr[0];
-		movie_obj.douban_score_2 = '';
-		if(score_arr.length>1)
-			movie_obj.douban_score_2 = score_arr[1];
-		//评分人数
-		movie_obj.douban_user = stringWithDot(s_obj.ratings_count);
+	if(typeof(id) === 'undefined' || id === '' || id === null ){
+		res.render('error');
+	} else {
+		var subtype = '';
+		var title = '';
+		superagent.get('https://api.douban.com/v2/movie/subject/'+id).end(function (err, m) {
+			var s_obj = JSON.parse(m.text);
+			var douban_url = s_obj.alt;
+			var movie_obj = {};
+			subtype = s_obj.subtype;
+			title = s_obj.title;
+			//链接
+			movie_obj.douban_url = s_obj.alt;
+			//封面图
+			movie_obj.douban_src = s_obj.images.large;
+			//评分
+			movie_obj.douban_score = s_obj.rating.average;
+			var score_arr = (s_obj.rating.average+'').split('.');
+			movie_obj.douban_score_1 = score_arr[0];
+			movie_obj.douban_score_2 = '';
+			if(score_arr.length>1)
+				movie_obj.douban_score_2 = score_arr[1];
+			//评分人数
+			movie_obj.douban_user = stringWithDot(s_obj.ratings_count);
 
-		var film_info = {};
-		//名称
-		film_info.title = s_obj.title;
-		//年代
-		film_info.year = s_obj.year;
-		//导演
-		film_info.directors;
-		//编剧
-		film_info.writers;
-		//主演
-		film_info.casts;
-		//影片类型
-		var g_arr = s_obj.genres;
-		var genres = '';
-		for(var g in g_arr){
-			genres += ' / ' + g_arr[g] ;
-		}
-		if(genres.length>0){
-			genres = genres.substring(3);
-		}
-		film_info.genres = genres;
-		//制片国家/地区
-		var c_arr = s_obj.countries;
-		var countries = '';
-		for(var c in c_arr){
-			countries += ' / ' + c_arr[c] ;
-		}
-		if(countries.length>0){
-			countries = countries.substring(3);
-		}
-		film_info.countries = countries;
-		//语言
-		film_info.languages;
-		//上映日期
-		film_info.pubdates;
-		//片长
-		film_info.durations;
-		//简介
-		film_info.summary = s_obj.summary;
-
-		superagent.get(douban_url).end(function (err, douban) {
-			var $ = cheerio.load(douban.text);
-			
-			//抓取短评
-			var s_comments = [];
-			$('div#hot-comments.tab div.comment-item').each(function(i,sc){
-				var s_comment = {};
-				var short_comment = $(sc);
-				var comment_content = short_comment.find('p').text();
-				var comment_vote = short_comment.find('span.comment-vote');
-				var comment_info = short_comment.find('span.comment-info').children();
-				var votes = comment_vote.children().first().html();
-				var user_url = comment_info.first().attr('href');
-				var user_name = comment_info.first().text();
-				var comment_time = comment_info.last().text().trim();
-				var rate = comment_info.first().next().attr('title');
-				s_comment.votes = votes;
-				s_comment.user_url = user_url;
-				s_comment.user_name = user_name;
-				s_comment.comment_time = comment_time;
-				s_comment.rate = rate;
-				s_comment.comment_content = comment_content;
-				s_comments.push(s_comment);
-			});
-
-
-
-			//抓取长评
-			var l_comments = [];
-			$('div#review_section div.review').each(function(i,lc){
-				var l_comment = {};
-				var long_comment = $(lc);
-				var comment_content = long_comment.find('div.review-bd').children().first().children().first().text();
-				var review_hd = long_comment.find('div.review-hd');
-				var comment_info = review_hd.children().first().children().last();
-				var comment_title = comment_info.attr('title');
-				var comment_url = comment_info.attr('href');
-				var review_hd_info = long_comment.find('div.review-hd-info').children();
-				var user_img = review_hd.children().first().children().first().find('img').attr('src');
-				var user_url = review_hd_info.first().attr('href');
-				var user_name = review_hd_info.first().text();
-				var rate = review_hd_info.last().attr('title');
-				l_comment.comment_content = comment_content;
-				l_comment.comment_title = comment_title;
-				l_comment.comment_url = comment_url;
-				l_comment.user_url = user_url;
-				l_comment.user_name = user_name;
-				l_comment.user_img = user_img;
-				l_comment.rate = rate;
-				l_comments.push(l_comment);
-			});
-
-			$('div#info span.attrs').each(function(i,e){
-				var ele = $(e);
-				var s = '';
-				ele.children('a').each(function(ai,ae){
-					var aele = $(ae);
-					s += ' / ' + aele.text() ;
-				});
-				if(s.length>0){
-					s = s.substring(3);
-				}
-				var p = ele.prev().text();
-				if(p === '导演'){
-					film_info.directors = s;
-				}
-				if(p === '编剧'){
-					film_info.writers = s;
-				}
-				if(p === '主演'){
-					film_info.casts = s;
-				}
-			});
-			
-			var imdb_url = '';
-			$('div#info span.pl').each(function(i,e){
-				var ele = $(e);
-				var k = ele.text();
-				var t = ele.next().text();
-				if(subtype === 'tv'){
-					if(k === '首播:'){
-						film_info.pubdates = t;
-					}
-				}else {
-					if(k === '上映日期:'){
-						film_info.pubdates = t;
-					}
-					if(k === '片长:'){
-						film_info.durations = t;
-					}
-				}
-				if(k === 'IMDb链接:'){
-					imdb_url = ele.next().attr('href');
-				}
-			});
-			
-			if(imdb_url !== ''){	
-				movie_obj.imdb_url = imdb_url;
+			var film_info = {};
+			//名称
+			film_info.title = s_obj.title;
+			//年代
+			film_info.year = s_obj.year;
+			//导演
+			film_info.directors;
+			//编剧
+			film_info.writers;
+			//主演
+			film_info.casts;
+			//影片类型
+			var g_arr = s_obj.genres;
+			var genres = '';
+			for(var g in g_arr){
+				genres += ' / ' + g_arr[g] ;
 			}
-			res.render('detail', {  l_comments:l_comments, s_comments:s_comments ,subtype:subtype,title:title, movie_obj:movie_obj , film_info:film_info });			
+			if(genres.length>0){
+				genres = genres.substring(3);
+			}
+			film_info.genres = genres;
+			//制片国家/地区
+			var c_arr = s_obj.countries;
+			var countries = '';
+			for(var c in c_arr){
+				countries += ' / ' + c_arr[c] ;
+			}
+			if(countries.length>0){
+				countries = countries.substring(3);
+			}
+			film_info.countries = countries;
+			//语言
+			film_info.languages;
+			//上映日期
+			film_info.pubdates;
+			//片长
+			film_info.durations;
+			//简介
+			film_info.summary = s_obj.summary;
+
+			superagent.get(douban_url).end(function (err, douban) {
+				var $ = cheerio.load(douban.text);
+				
+				//抓取短评
+				var s_comments = [];
+				$('div#hot-comments.tab div.comment-item').each(function(i,sc){
+					var s_comment = {};
+					var short_comment = $(sc);
+					var comment_content = short_comment.find('p').text();
+					var comment_vote = short_comment.find('span.comment-vote');
+					var comment_info = short_comment.find('span.comment-info').children();
+					var votes = comment_vote.children().first().html();
+					var user_url = comment_info.first().attr('href');
+					var user_name = comment_info.first().text();
+					var comment_time = comment_info.last().text().trim();
+					var rate = comment_info.first().next().attr('title');
+					s_comment.votes = votes;
+					s_comment.user_url = user_url;
+					s_comment.user_name = user_name;
+					s_comment.comment_time = comment_time;
+					s_comment.rate = rate;
+					s_comment.comment_content = comment_content;
+					s_comments.push(s_comment);
+				});
+
+
+
+				//抓取长评
+				var l_comments = [];
+				$('div#review_section div.review').each(function(i,lc){
+					var l_comment = {};
+					var long_comment = $(lc);
+					var comment_content = long_comment.find('div.review-bd').children().first().children().first().text();
+					var review_hd = long_comment.find('div.review-hd');
+					var comment_info = review_hd.children().first().children().last();
+					var comment_title = comment_info.attr('title');
+					var comment_url = comment_info.attr('href');
+					var review_hd_info = long_comment.find('div.review-hd-info').children();
+					var user_img = review_hd.children().first().children().first().find('img').attr('src');
+					var user_url = review_hd_info.first().attr('href');
+					var user_name = review_hd_info.first().text();
+					var rate = review_hd_info.last().attr('title');
+					l_comment.comment_content = comment_content;
+					l_comment.comment_title = comment_title;
+					l_comment.comment_url = comment_url;
+					l_comment.user_url = user_url;
+					l_comment.user_name = user_name;
+					l_comment.user_img = user_img;
+					l_comment.rate = rate;
+					l_comments.push(l_comment);
+				});
+
+				$('div#info span.attrs').each(function(i,e){
+					var ele = $(e);
+					var s = '';
+					ele.children('a').each(function(ai,ae){
+						var aele = $(ae);
+						s += ' / ' + aele.text() ;
+					});
+					if(s.length>0){
+						s = s.substring(3);
+					}
+					var p = ele.prev().text();
+					if(p === '导演'){
+						film_info.directors = s;
+					}
+					if(p === '编剧'){
+						film_info.writers = s;
+					}
+					if(p === '主演'){
+						film_info.casts = s;
+					}
+				});
+				
+				var imdb_url = '';
+				$('div#info span.pl').each(function(i,e){
+					var ele = $(e);
+					var k = ele.text();
+					var t = ele.next().text();
+					if(subtype === 'tv'){
+						if(k === '首播:'){
+							film_info.pubdates = t;
+						}
+					}else {
+						if(k === '上映日期:'){
+							film_info.pubdates = t;
+						}
+						if(k === '片长:'){
+							film_info.durations = t;
+						}
+					}
+					if(k === 'IMDb链接:'){
+						imdb_url = ele.next().attr('href');
+					}
+				});
+				
+				if(imdb_url !== ''){	
+					movie_obj.imdb_url = imdb_url;
+				}
+				res.render('detail', {  l_comments:l_comments, s_comments:s_comments ,subtype:subtype,title:title, movie_obj:movie_obj , film_info:film_info });			
+			});
 		});
-	});
+	}
 });
 
 function stringWithDot(s){
