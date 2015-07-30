ef02abe85f7e626c222e17a60b79a679d1dda309
panhb <391400311@qq.com>
2015-07-30 11:31:45 +0800

    weixin fix

--- public/js/index.js
+++ public/js/index.js
@@ -4,10 +4,12 @@ function showWeixin(obj){
 		placement:placement,
 		//animation:true,
 		html:true,
-		content:'<img  width="256" height="256" src="/images/weixin.png">'
+		//content:'<img  width="256" height="256" src="/images/weixin.png">'
+		content:'<img  width="192" height="192" src="/images/weixin.jpg">'
 	})
 	$(obj).popover('show');
-	$('.popover-content').css('padding','4px');
+	//$('.popover-content').css('padding','4px');
+	$('.popover-content').css('padding','0px');
 }
 
 function hideWeixin(obj){
